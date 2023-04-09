9a7cd711d84fa21cc4d0b6514e4a0ba9d0500e38
Long Hoang Tran <lanimatran@gmail.com>
2023-04-09 14:47:04 -0600

    upload classifier

---
+++ src/classifier.py
@@ -0,0 +1,93 @@
+import keras
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers import Layer, Dense, Activation, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import BatchNormalization
+from keras.utils import to_categorical
+
+from load_data import load_data, load_labels
+
+import numpy as np
+
+train_data = np.array(load_data("train", 32))
+train_label = np.array(load_labels("train"))
+test_data = np.array(load_data("test", 32))
+test_label = np.array(load_labels("test"))
+
+def get_model():
+    model = Sequential()
+    
+    model.add(Conv2D(32, (3, 3), padding='same',
+                    input_shape=train_data.shape[1:]))
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+    
+    model.compile(loss='binary_crossentropy',
+                    optimizer='adam',
+                    metrics=['accuracy'])
+  
+    return model
+
+def get_model2():
+    model = Sequential()
+    
+    model.add(Conv2D(32, (3, 3), padding='same',
+                    input_shape=train_data.shape[1:]))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+    
+    model.compile(loss='binary_crossentropy',
+                    optimizer='adam',
+                    metrics=['accuracy'])
+  
+    return model
+
+def train_model():
+    batch_size = 32
+    epochs = 25
+    model = get_model2()
+
+    model.fit(
+        train_data, train_label,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=(test_data, test_label),
+        shuffle=True
+    )
+
+    model.save(str('D:/U-of-Wyo-ML-5555-01-22772-Spring-2023-Research-Project/src/model2.h5'))
+
+def load_model():
+    model = keras.models.load_model(str('D:/U-of-Wyo-ML-5555-01-22772-Spring-2023-Research-Project/src/model2.h5'))
+    test_data = np.array(load_data("test", 32))
+    test_label = np.array(load_labels("test"))
+    scores = model.evaluate(test_data, test_label, verbose=1)
+    print('Test accuracy:', scores[1])
+
+train_model()
